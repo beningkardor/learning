3bf3bcc0f3c6c7b0e55da95f7f86a87e581279d4
beningkardor <beningkardor@gmail.com>
2026-08-09 14:16:29 +0800

    Add offline cache service worker

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "money-tracker-v1-1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
